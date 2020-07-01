3f939acc4313e1da1629405d6d1e0e4723da17ea
Johnny Valles <vallesja@uci.edu>
2020-07-01 01:01:23 -0700

    Create test clear command

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <string.h>
 
 #define MAXARGS 128
 #define MAXLINE 8192
@@ -11,9 +13,12 @@ void _print_buffer_(const char* buf) {
 
 int main(int argc, char** argv) {
 
+    int i;
+    struct winsize terminal;
     char cmdline[MAXLINE];
     
     while(1) {
+        printf("ROWS: %d, COLUMNS: %d\n", terminal.ws_row, terminal.ws_col);
         printf(">> ");
         
         fgets(cmdline, MAXLINE, stdin);
@@ -22,6 +27,15 @@ int main(int argc, char** argv) {
         // Check if user typed CTRL+d to exit shell
         if(feof(stdin))
             exit(0);
+
+        if (strcmp(cmdline, "clear")) {
+            ioctl(0, TIOCGWINSZ, &terminal);
+            
+            for (i = 0; i < terminal.ws_row - 2; ++i)
+                printf("\n");
+
+        }
+
 
     }
 
