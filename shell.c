d4e8fac300c34ba2413886b39ffdf86b694e62eb
Johnny Valles <vallesja@uci.edu>
2020-07-10 23:49:52 -0700

    Rename main.c to shell.c

---
+++ shell.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXARGS 128
+#define MAXLINE 8192
+
+int
+main() {
+    char buf[MAXLINE];                                      /* buffer holds user's input */
+    while(1) {
+        printf(">>> ");                                     /* print the shell prompt */
+        fgets(buf, MAXLINE, stdin);                         /* read a line and store in buffer */
+        if (feof(stdin))                                    /* check for end-of-file */
+            exit(0);
+        printf("%s\n", buf);
+    }
+    return EXIT_SUCCESS;
+}
